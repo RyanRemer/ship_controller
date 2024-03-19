3bc18078363c54b49573fd490ad708eb7cda84e2
Ryan Remer <ryandremer@gmail.com>
2024-03-18 20:57:20 -0700

    move components to ship_controller directory

--- ship_controller/flames.gd
+++ ship_controller/flames.gd
@@ -4,6 +4,7 @@ class_name Flames extends Node3D
 @export var flame_color = Color.from_string("00fff7", Color.WHITE):
 	set(color):
 		flame_color = color;
+		flame_mesh.material_override.albedo_color = color;
 		flame_mesh.material_override.emission = flame_color;
 		
 @onready var flame_mesh = $FlameMesh;
